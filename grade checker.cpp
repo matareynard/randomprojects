e4afaf8d2d3006061409274939dc566e69d34e23
reynard <148642816+matareynard@users.noreply.github.com>
2025-04-21 16:49:37 +0800

    Add files via upload

---
+++ grade checker.cpp
@@ -0,0 +1,13 @@
+include <iostream>
+using namespace std;
+int main(){
+ int grade;
+ cout << "enter grade: ";
+ cin >> grade;
+ if(grade<=74 && grade>=50){
+ cout << "bobo, see yah next year: " << endl;}
+else if(grade >=75){
+cout << "you passed, bitch. ";}
+else {cout<<"stap sag eskwela";}
+return 0;
+}
---
+++ loadslip.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+                   cout << "                        Student Number : 202303562.                                            Scholarship:"<<endl;
+                   cout << "                                  Name : Mata, Reynard V.                                      Amount paid : 0.00"<< endl;
+                   cout << "                  Date Of Birth/Gender : 2004-3-29(M)                                           OR numbers : /" << endl;
+                   cout << "                        College/Course : Bachelor Of Science In Computer Science(CAS).           Date paid : 2023-8-18/2023-8-18" << endl;
+                   cout << "                            Major/Minor:                                                   Yr Level/status : 1-new" << endl;
+cout << "----------------------------------------------------------------------------------------------------------------------------------------------------------------------"<< endl;
+                   cout << "                  Subject    Sec Description                                        Schedule                      Room                            Units" << endl;
+                   cout << "            1 FIL 1" << endl;
+                   cout << "            2 GE  6"   << endl;
+                   cout << "            3 EN COMP " << endl;
+                   cout << "            4 CSC 101 " << endl;
+                   cout << "            5 CSC 100" << endl;
+                   cout << "            6 GE  4"<< endl;
+                   cout << "            7 NSTP-1AFROTC" << endl;
+                   cout << "            8 PE  1" << endl;
+                   cout << "            9 EN PRECAL" << endl;
+                   cout << "           10 GE  5" << endl;
+
+    return 0;
+}
+
+
